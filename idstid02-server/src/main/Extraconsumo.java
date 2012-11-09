19ae9a5bbd9b4152fc0bc1dcde8770ad809b1e4e
katypierangeli <katypierangeli@localhost>
2012-11-09 19:29:27 +0100

    librerie forms

---
+++ idstid02-server/src/main/Extraconsumo.java
@@ -0,0 +1,55 @@
+package main;
+
+public class Extraconsumo {
+	private int id;
+	private double quantita;
+	private boolean giustificato;
+	private String dataRichiesta;
+	
+	public Extraconsumo(int id, double quantita, boolean giustificato,
+			String dataRichiesta) {
+		this.id = id;
+		this.quantita = quantita;
+		this.giustificato = giustificato;
+		this.dataRichiesta = dataRichiesta;
+	}
+	
+	public Extraconsumo() {
+		this.id = -1;
+		this.quantita = 0;
+		this.giustificato = false;
+		this.dataRichiesta = "";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getQuantita() {
+		return quantita;
+	}
+
+	public void setQuantita(double quantita) {
+		this.quantita = quantita;
+	}
+
+	public boolean isGiustificato() {
+		return giustificato;
+	}
+
+	public void setGiustificato(boolean giustificato) {
+		this.giustificato = giustificato;
+	}
+
+	public String getDataRichiesta() {
+		return dataRichiesta;
+	}
+
+	public void setDataRichiesta(String dataRichiesta) {
+		this.dataRichiesta = dataRichiesta;
+	}
+}
