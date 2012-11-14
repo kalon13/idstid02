59de574aeb6fd1fc0ce608c3b6ada469c3a12d97
katypierangeli <katypierangeli@localhost>
2012-11-14 16:57:29 +0100

    Backed out of changeset 23:20c5f93dae72

---
+++ idstid02-client/src/ResourceClass.java
@@ -0,0 +1,134 @@
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.net.URI;
+import java.util.List;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriBuilder;
+
+import classResources.Materiale;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.GenericType;
+import com.sun.jersey.api.client.UniformInterfaceException;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+
+
+public class ResourceClass {
+	
+
+	public ResourceClass(){	}
+		
+		private static Client client = null;  
+		public static WebResource service = null;
+		
+		private static void Config(){ 
+		  ClientConfig config = new DefaultClientConfig();
+		  client = Client.create(config);
+	      service = client.resource(getBaseURI());
+	  	}
+		//per visualizzare tutti i dati 
+		public static <T> List<T> getResources(final Class<T> clazz, String path) throws UniformInterfaceException { 
+			   ParameterizedType genericType = new ParameterizedType() { 
+	               @Override 
+	               public Type[] getActualTypeArguments() { 
+	                   return new Type[] {clazz}; 
+	               } 
+
+	               @Override 
+	               public Type getRawType() { 
+	                   return List.class; 
+	               } 
+
+	               @Override 
+	               public Type getOwnerType() { 
+	                   return List.class; 
+	               } 
+	           };
+	           if (service == null && client == null ) Config();
+	           GenericType<List<T>> type = new GenericType<List< T >>(genericType) {};
+	           return service.path(path).accept(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(type);
+	     } 
+		//per visualizzare un det dato passare nella path/id del dato stesso
+		public static <T> T getResource(final Class<T> clazz, String path) { 
+			   if (service == null && client == null ) Config();
+			   T t = service.path(path).accept(MediaType.APPLICATION_JSON).get(clazz);
+			   return t;
+	     } 
+	
+		//TODO: Aggiunge un nuova risorsa		
+		public static <T> String addResources(String path, T classObj) throws UniformInterfaceException { 
+			if (service == null && client == null ) Config();
+			String className = classObj.getClass().getName(); 
+			MultivaluedMap<String, String> formData = new MultivaluedMapImpl();
+			formData = multValueIns(className, classObj, path);
+		    String id = service.path(path)
+		    		.accept(MediaType.APPLICATION_JSON)
+		    		.type(MediaType.APPLICATION_FORM_URLENCODED).put(String.class, formData);
+		    return id;
+		} 
+		
+		//TODO: Modifico una risorsa		
+		public static <T> void updResources(final Class<T> clazz, String path, String id, T classObj) throws UniformInterfaceException { 
+			if (service == null && client == null ) Config();
+			String className = clazz.getName();
+			MultivaluedMap<String, String> formData = new MultivaluedMapImpl();
+			formData = multValueUpd(className, classObj, path);
+			System.out.print(id);
+			service.path(path).path(id).accept(MediaType.APPLICATION_JSON).post(clazz, formData);
+		} 
+		
+		// Elimino una risorsa		
+		public static void delResources(String path, String id) throws UniformInterfaceException { 
+			if (service == null && client == null ) Config();
+		    service.path(path).path(id).accept(MediaType.APPLICATION_JSON).delete(String.class);
+		} 
+		
+		private static <T> MultivaluedMap<String, String> multValueUpd(String className, T classObj, String path){
+		 MultivaluedMap<String, String> formData = new MultivaluedMapImpl();
+		  if (className == "classResources.Materiale" && path.equals(Global._URLMag)){
+				Materiale m = (Materiale) classObj;
+				String quantita =  String.valueOf(m.getQuantita());
+				formData.add("quantita", quantita) ;
+				System.out.print(quantita);
+			}
+		  else if (className == "classResources.Materiale"  && path.equals(Global._URLddt)){
+				Materiale m = (Materiale) classObj;
+				String quantita =  String.valueOf(m.getQuantita());
+				formData.add("quantita", quantita);
+			}
+			return formData;
+	   }
+	   private static <T> MultivaluedMap<String, String> multValueIns(String className, T classObj, String path){
+			 MultivaluedMap<String, String> formData = new MultivaluedMapImpl();
+			 if (className == "classResources.Materiale" && path.equals(Global._URLMag)){
+					Materiale m = (Materiale) classObj;
+					String des = m.getDescrizione();
+					String costUn =  String.valueOf(m.getCostoUnitario());
+					formData.add("descrizione", des);
+					formData.add("costoUnitario", costUn) ;
+					System.out.print(des+costUn);
+				}
+			  else if (className == "classResources.Materiale"  && path.equals(Global._URLddt)){
+					Materiale m = (Materiale) classObj;
+					String id = String.valueOf(m.getId());
+					String id_terzista =  String.valueOf(m.getId_terzista());
+					String quantita =  String.valueOf(m.getQuantita());
+					formData.add("Materiale_id", (id));
+					formData.add("Terzista_id", id_terzista);
+					formData.add("quantita", quantita);
+				}
+				return formData;
+		   }
+		
+	public static URI getBaseURI() {
+		return UriBuilder.fromUri("http://localhost:8080/idstid02-server").build();
+	}
+
+
+}
---
+++ idstid02-server/src/main/Messaggio.java
@@ -0,0 +1,58 @@
+package main;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Messaggio {
+	private int id;
+	private String data;
+	private String testo;
+	private boolean letto;
+	//TODO: aggiungere utente_Id
+	//TODO: aggiungere bolla_Id
+	
+	public Messaggio(int id, String data, String testo, boolean letto) {
+		this.id = id;
+		this.data = data;
+		this.testo = testo;
+		this.letto = letto;
+	}
+	
+	public Messaggio() {
+		this.id = -1;
+		this.data = "";
+		this.testo = "";
+		this.letto = false;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getTesto() {
+		return testo;
+	}
+
+	public void setTesto(String testo) {
+		this.testo = testo;
+	}
+
+	public boolean isLetto() {
+		return letto;
+	}
+
+	public void setLetto(boolean letto) {
+		this.letto = letto;
+	}
+}
