d6845643f967bac7dbcddf8bc012b4478f935983
Davide Marzioni <d.marzioni@idea-on-line.it>
2012-11-24 09:41:34 +0100

    Merge

---
+++ idstid02-client/src/Global.java
@@ -0,0 +1,11 @@
+public class Global {
+	public static final String _URLMag = "/magazzinoterzista";
+	public static final String _URLMagSearch = "/magazzinoterzista/search/";
+	public static final String _URLMagMat = "/magazzinoterzista/materiale/";
+	public static final String _URLFatt = "/fatturazione";
+	public static final String _URLBolla = "/bolla";
+	public static final String _URLAut = "/autenticazione";
+	public static final String _URLddt = "/DDT";
+	public static final String _URLMess = "/messaggio";
+	public static final String _URLMessSearch = "/messaggio/search/";
+}
