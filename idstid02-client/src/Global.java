b44cf73b1e17a686451e2227474a787fe39d819d
katypierangeli <katypierangeli@localhost>
2012-11-24 11:21:01 +0100

    package main nel client

--- idstid02-client/src/Global.java
+++
@@ -1,9 +0,0 @@
-public class Global {
-	public static final String _URLMag = "/magazzinoterzista";
-	public static final String _URLMagSearch = "/magazzinoterzista/search/";
-	public static final String _URLMagMat = "/magazzinoterzista/materiale/";
-	public static final String _URLFatt = "/fatturazione";
-	public static final String _URLBolla = "/bolla";
-	public static final String _URLAut = "/autenticazione";
-	public static final String _URLddt = "/DDT";
-}
--- idstid02-client/src/main/GUI_Home.java
+++ idstid02-client/src/main/GUI_Home.java
@@ -1,3 +1,4 @@
+package main;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JPanel;
