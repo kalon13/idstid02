351d535406687f4347bade2bf1abdd92d108ab61
Davide Marzioni <d.marzioni@idea-on-line.it>
2012-11-26 08:26:30 +0100

    hg ora ignora i file .classpath Le librerie che servono per il client e per il server sono: - Glassfish - JDK - EAP

---
+++ idstid02-client/src/main/Global.java
@@ -0,0 +1,14 @@
+package main;
+public class Global {
+        public static final String _URLMag = "/magazzinoterzista";
+        public static final String _URLMagSearch = "/magazzinoterzista/search/";
+        public static final String _URLMagMat = "/magazzinoterzista/materiale/";
+        public static final String _URLFatt = "/fatturazione";
+        public static final String _URLBolla = "/bolla";
+        public static final String _URLUser = "/utente";
+        public static final String _URLAutLogin = "/autenticazione/login";
+        public static final String _URLAutLogout = "/autenticazione/logout";
+        public static final String _URLddt = "/DDT";
+        public static final String _URLMess = "/messaggio";
+        public static final String _URLMessSearch = "/messaggio/search/";
+}
