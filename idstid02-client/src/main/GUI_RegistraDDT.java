0a7c4ba41f9eec4fee8c0c5a700203911927b9a2
katypierangeli <katypierangeli@localhost>
2012-12-23 19:06:19 +0100

    mod

--- idstid02-client/src/main/GUI_RegistraDDT.java
+++ idstid02-client/src/main/GUI_RegistraDDT.java
@@ -50,9 +50,7 @@ public class GUI_RegistraDDT {
              loadLstDDT(idTerz);
              initialize();
         }
-                /**
-         * Initialize the contents of the frame.
-         */
+       
         private void initialize() {
                 frmRegistraDdt = new JFrame();
                 frmRegistraDdt.setResizable(false);
