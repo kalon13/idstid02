4c92ad79e58b14eb4464cfa6ab92a28675c57633
giorgia <giorgia@localhost>
2012-12-28 18:06:48 +0100

    Merge

---
+++ idstid02-client/src/main/GUI_AggiungiMorti.java
@@ -0,0 +1,94 @@
+package main;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPanel;
+import javax.swing.JComboBox;
+import javax.swing.JList;
+import javax.swing.JButton;
+
+
+public class GUI_AggiungiMorti {
+
+        private JFrame frmAggiungiMorti;
+        private JTextField txtNumBolla;
+        private JTextField txtMortiProdotti;
+
+        /**
+         * Launch the application.
+         */
+        public static void main(String[] args) {
+                EventQueue.invokeLater(new Runnable() {
+                        public void run() {
+                                try {
+                                        GUI_AggiungiMorti window = new GUI_AggiungiMorti();
+                                        window.frmAggiungiMorti.setVisible(true);
+                                } catch (Exception e) {
+                                        e.printStackTrace();
+                                }
+                        }
+                });
+        }
+
+        /**
+         * Create the application.
+         */
+        public GUI_AggiungiMorti() {
+                initialize();
+        }
+
+        /**
+         * Initialize the contents of the frame.
+         */
+        private void initialize() {
+                frmAggiungiMorti = new JFrame();
+                frmAggiungiMorti.setResizable(false);
+                frmAggiungiMorti.setTitle("Aggiungi Morti");
+                frmAggiungiMorti.setBounds(100, 100, 271, 265);
+                frmAggiungiMorti.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                frmAggiungiMorti.getContentPane().setLayout(null);
+               
+                JLabel lblBollaN = new JLabel("Bolla n\u00B0:");
+                lblBollaN.setBounds(10, 11, 46, 14);
+                frmAggiungiMorti.getContentPane().add(lblBollaN);
+               
+                txtNumBolla = new JTextField();
+                txtNumBolla.setEditable(false);
+                txtNumBolla.setBounds(73, 8, 77, 20);
+                frmAggiungiMorti.getContentPane().add(txtNumBolla);
+                txtNumBolla.setColumns(10);
+               
+                JPanel panel = new JPanel();
+                panel.setBounds(10, 36, 243, 152);
+                frmAggiungiMorti.getContentPane().add(panel);
+                panel.setLayout(null);
+               
+                JLabel lblMateriali = new JLabel("Materiali:");
+                lblMateriali.setBounds(10, 11, 46, 14);
+                panel.add(lblMateriali);
+               
+                JLabel lblMortiProdotti = new JLabel("Morti Prodotti:");
+                lblMortiProdotti.setBounds(119, 25, 99, 14);
+                panel.add(lblMortiProdotti);
+               
+                JList listMateriali = new JList();
+                listMateriali.setBounds(10, 30, 99, 111);
+                panel.add(listMateriali);
+               
+                JButton btnAggiungiMorti = new JButton("Aggiungi Morti");
+                btnAggiungiMorti.setBounds(119, 75, 114, 23);
+                panel.add(btnAggiungiMorti);
+               
+                txtMortiProdotti = new JTextField();
+                txtMortiProdotti.setBounds(119, 44, 114, 20);
+                panel.add(txtMortiProdotti);
+                txtMortiProdotti.setColumns(10);
+               
+                JButton btnEsci = new JButton("Esci");
+                btnEsci.setBounds(164, 201, 89, 23);
+                frmAggiungiMorti.getContentPane().add(btnEsci);
+        }
+}
+
---
+++ idstid02-client/src/main/Global.java
@@ -0,0 +1,43 @@
+package main;
+
+public class Global {
+    public static final String _URLMag = "/magazzinoterzista";
+    public static final String _URLMagSearch = "/magazzinoterzista/search/";
+    public static final String _URLMagMat = "/magazzinoterzista/materiale/";
+    public static final String _URLMagMatTerz = "/magazzinoterzista/matTerzista/";
+    public static final String _URLMagUpdMat = "/magazzinoterzista/updMat/";
+    public static final String _URLFatt = "/fatturazione";
+    public static final String _URLFattTerz = "/fatturazione/terzista/";
+    public static final String _URLBolla = "/bolla";
+    public static final String _URLBollaValuta = "/bolla/valuta/";
+//    public static final String _URLBollaTerz = "/bolla/search/";
+    //public static final String _URLAut = "/autenticazione";
+    public static final String _URLddt = "/DDT";
+    public static final String _URLddtNnReg = "/DDT/DDTnnReg/";
+    public static final String _URLddtTerz = "/DDT/Terzista/"; 
+    public static final String _URLddtInsMat = "/MaterialeDDT";
+    public static final String _URLConsMat = "/consumo/";
+    public static final String _URLTerz = "/terzista/";
+    public static final String _URLUser = "/utente";
+    public static final String _URLMatTeoSearch = "/matTeo/search/"; //con join
+    public static final String _URLMatId = "/materiale";
+    public static final String _URLMatDaProdurre = "/matDaProdurre";
+    public static final String _URLAutLogin = "/autenticazione/login";
+    public static final String _URLAutLogout = "/autenticazione/logout";
+    public static final String _URLMess = "/messaggio";
+    public static final String _URLMessSearch = "/messaggio/search/";
+    public static final String _URLLavoraz = "/lavorazione/";
+    public static final String _URLLavorazTerzista = "/lavorazioneterzista/";
+    public static final String _URLFase = "/fase/";
+    public static final String _URLMatDaProd1 = "/matDaProdurre/idBolla/"; //con join
+    public static final String _URLBollaTerz = "/bolla/terzista/"; //Giorgia
+    public static final String _URLBollaStato = "/bolla/stato/"; //Giorgia Post
+    public static final String _URLBollaCM = "/bolla/statoCM/"; //Giorgia (Bolla Chiusa o con Morto)
+    public static final String _URLExtra = "/extraconsumo/idBolla/"; //Giorgia
+    public static final String _URLExtraPost = "/extraconsumo"; //Giorgia (Post di Extraconsumo)
+    public static final String _URLExtraIns = "/extraconsumo"; //Giorgia (Insert di Extraconsumo)
+    public static final String _URLPaia = "/paia/idBolla/"; //Giorgia
+    public static final String _URLBollaRiassegna = "/bolla/riassegna/";
+    public static final String _URLMatDaProdSped = "/matDaProdurre/spedita/";
+}
+
